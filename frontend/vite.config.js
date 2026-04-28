faab7a2fd0e50ccffcacf53bf253d4276607bc39
UpsilonX-devsite <kiddoyles@gmail.com>
2026-04-28 15:34:35 +0800

    Update vite.config.js
    Added     allowedHosts: ['general-taowar-production.up.railway.app'],

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    allowedHosts: ['general-taowar-production.up.railway.app'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
